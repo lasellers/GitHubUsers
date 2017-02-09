7c236b3e1c689eb5467693625413e48690aff71c
Lewis A. Sellers <lasellers@gmail.com>
2017-02-08 23:29:37 -0500

    Tweak css

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -7,7 +7,6 @@ export class HighlightDirective {
 
   constructor(el: ElementRef) {
     el.nativeElement.style.backgroundColor = '#eee';
-   /* el.nativeElement.style.color = 'gold';*/
     console.log(`highlight called on ${el.nativeElement.tagName}`);
    }
 
