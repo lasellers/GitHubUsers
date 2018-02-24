47bb169501bfe5ac9f3cba3acd77f6ed230019b3
Lewis A. Sellers <lasellers@gmail.com>
2018-02-23 21:53:00 -0500

    move user detail to component

---
+++ src/app/highlight2.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[highlight2]'
+})
+export class Highlight2Directive implements OnInit{
+
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  )
+  {
+  }
+
+  ngOnInit() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', "AntiqueWhite ");
+   // console.log(`highlight2 called on ${this.renderer.}`);
+  }
+}
---
+++ src/app/users/user-detail/user-detail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { GitHubUserService } from '../../git-hub-user.service';
+
+@Component({
+  selector: 'user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
+})
+export class UserDetailComponent implements OnInit {
+
+  constructor(   
+    private userService: GitHubUserService
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
