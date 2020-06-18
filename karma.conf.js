c3f08088a39ea1d1a14c5b7e9d9ae0bb122225f2
Lewis Sellers <lewis.sellers@clearwatercompliance.com>
2020-06-18 17:43:50 -0400

    Update coverage reports

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,10 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-summary-reporter'),
+      require('karma-spec-reporter'),
+      require('karma-html-detailed-reporter')
     ],
     files: [
       'src/app/*.spec.ts',
@@ -30,9 +33,30 @@ module.exports = function (config) {
           subdir: 'html'
         }
       },
-      verbose: true
+      verbose: false
     },
-    reporters: ['progress', 'kjhtml', 'coverage-istanbul'],
+    summaryReporter: {
+      // 'failed', 'skipped' or 'all'
+      show: 'failed',
+      // Limit the spec label to this length
+      specLength: 50,
+      // Show an 'all' column as a summary
+      overviewColumn: true
+    },
+    specReporter: {
+      maxLogLines: 5,             // limit number of lines logged per test
+      suppressErrorSummary: true, // do not print error summary
+      suppressFailed: false,      // do not print information about failed tests
+      suppressPassed: false,      // do not print information about passed tests
+      suppressSkipped: true,      // do not print information about skipped tests
+      showSpecTiming: false,      // print the time elapsed for each spec
+      failFast: true              // test would finish with error when a first fail occurs.
+    },
+    htmlDetailed: {
+      splitResults: true
+    },
+    reporters: ['dots', 'kjhtml', 'coverage-istanbul', 'spec'],
+    // reporters: ['progress', 'kjhtml', 'coverage-istanbul', 'summary', 'spec', 'htmlDetailed'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
