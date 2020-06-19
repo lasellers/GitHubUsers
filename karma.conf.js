e4b451286e67937e632801c592d13158a24f0292
Lewis Sellers <lasellers@gmail.com>
2020-06-19 19:00:42 -0400

    Update circleci

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     concurrency: Infinity,
     singleRun: false,
     restartOnFileChange: true,
