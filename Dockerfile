3dcc05bdbe3840f6c7fc275c98d28384c1f7ddb4
Lewis Sellers <lasellers@gmail.com>
2020-06-20 07:14:08 -0400

    Update circleci

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
 # sudo docker build -t githubusers .
 # sudo docker run -it githubusers
-# FROM ubuntu:18.04
 FROM circleci/node:12-browsers
-# USER node
-USER circleci
-RUN usermod -aG sudo circleci
 WORKDIR /app
-COPY . /app
-RUN chown circleci:circleci * -R
-# USER docker
-# RUN npm i
+COPY --chown=circleci:circleci . /app
+# RUN chown circleci:circleci * -R
+RUN npm i
 CMD bash
 
--- test/test.spec.js
+++ test/test.spec.js
@@ -3,17 +3,17 @@ const should = chai.should();
 const expect = chai.expect;
 const assert = chai.assert;
 
-describe("hello world tests", function() {
+describe("hello world tests", function () {
 
-  it("assert test", function() {
+  it("assert test", function () {
     assert.equal(2, 1 + 1);
   });
 
-  it("expect test", function() {
+  it("expect test", function () {
     expect(1 + 1).to.equal(2);
   });
 
-  it("should test", function() {
+  it("should test", function () {
     const calc = 1 + 1;
     calc.should.equal(2);
   });
